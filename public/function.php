e08317771ef61b433b79950e6c56b2a88c3ce1b1
mariusz88maniek88 <mariuszborkowski1988@tutanota.com>
2017-10-07 14:32:28 +0200

    0.2.1

--- public/function.php
+++ public/function.php
@@ -23,5 +23,30 @@ function show_from() {
 }
 
 
+function save_hours() {
+    
+    if (isset($_GET['zapisz_godziny'])) {
+        
+        $date = $_GET['data'];
+        $poczatek_pracy = $_GET['rozpoczecie_pracy'];
+        $koniec_pracy = $_GET['koniec_pracy'];
+        $poczatek_przerwy = $_GET['czas_rozp_prz'];
+        $koniec_przerwy = $_GET['czas_kon_prz'];
+        
+        if( !empty($date) && !empty($poczatek_pracy) && !empty($koniec_pracy) && !empty($poczatek_przerwy) && !empty($koniec_przerwy) ) {
+            
+            echo 'Wszystko OK';
+            
+        } else {
+            
+            echo '<h2>Wypełnij pola</h2>';
+            
+        }
+        
+    }
+    
+}
+
+
 
 ?>
